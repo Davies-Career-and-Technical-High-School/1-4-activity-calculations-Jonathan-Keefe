e61a8185280421fde2379386f3feefbb8d3eab7e
Jonathan Keefe <jonathan.keefe@daviestech.org>
2020-10-07 15:13:44 +0000

    Question 4

--- Question4.java
+++ Question4.java
@@ -4,6 +4,23 @@ class Question4 {
     public static void main(String[] args) {
 
         /* Write your code here */
+      Scanner scan = new Scanner(System.in);
+      double price;
+      double bill;
+      double owed;
+    
+      System.out.println("Enter a price");
+      price = scan.nextDouble();
+      bill = 20;
+      owed = bill - price;
+      
+      
+      
+      
 
+
+
+      System.out.println("Change from $20: $" + owed);
+      
     }
 }
