aec4a8776f0ded90dab7740536448b696c96836f
Jonathan Keefe <jonathan.keefe@daviestech.org>
2020-10-07 15:00:22 +0000

    Question 1 and 2

--- Question2.java
+++ Question2.java
@@ -4,6 +4,21 @@ class Question2 {
     public static void main(String[] args) {
 
         /* Write your code here */
+      Scanner scan = new Scanner(System.in);
+      int inches;
+      int feet;
+      int num;
+      System.out.println("Enter number of inches");
+      inches = scan.nextInt();
+      num = 12;
+      feet = inches / num;
+      
+      
+      
 
+
+
+      System.out.println(feet);
+      
     }
 }
