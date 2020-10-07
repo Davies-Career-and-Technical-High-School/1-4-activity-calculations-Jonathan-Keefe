e038b9593c960403c8ed9900eee82d5ab1c4d3ea
Jonathan Keefe <jonathan.keefe@daviestech.org>
2020-10-07 15:09:38 +0000

    Question 3

--- Question3.java
+++ Question3.java
@@ -4,6 +4,23 @@ class Question3 {
     public static void main(String[] args) {
 
         /* Write your code here */
+      Scanner scan = new Scanner(System.in);
+      double radius;
+      double circ;
+      double area;
+      double pi;
+      System.out.println("Enter radius");
+      radius = scan.nextInt();
+      pi = 3.14;
+      circ = 2 * pi * radius;
+      area = pi * (radius * radius);
+      
+      
+      
 
+
+
+      System.out.println(circ);
+      System.out.println(area);
     }
 }
